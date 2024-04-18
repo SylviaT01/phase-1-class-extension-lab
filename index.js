ccd8da990139834d436c2cbc310ecf799a454ddf
Sylvia <sylviachebet03@gmail.com>
2024-04-18 17:25:02 +0300

    Write JavaScript classes That Inherit From Other Classes

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Your code here
+class Polygon {
+    constructor(sides){
+        this.sides = sides;
+    }
+    get countSides(){
+        return this.sides.length;
+    }
+    get perimeter(){
+        return this.sides.reduce((total, side) => total + side, 0);
+    }
+}
+class Triangle extends Polygon {
+    constructor(sides){
+        super(sides);
+    }
+    get isValid(){
+        if (this.countSides !== 3) {
+            return false;
+          }
+          const [a, b, c] = this.sides;
+          return a + b > c && a + c > b && b + c > a;
+    }
+}
+class Square extends Polygon{
+    constructor(sides){
+        super(sides);
+    }
+    get isValid(){
+        if (this.countSides !== 4){
+            return false;
+        }
+        const [a, b, c, d] = this.sides;
+        return a === b && b === c && c === d;
+    }
+    get area(){
+        if (!this.isValid){
+            return null;
+        }
+        const sideLength = this.sides[0];
+        return sideLength ** 2;
+    }
+}
